13ab81d99eafb4638e5af3faf7ca8c17fbb6006c
Dimalsha Perera <msdperera99@gmail.com>
2026-06-07 21:56:10 +0530

    Error FIxed 1

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
-}
-
 let cached = (global as any).mongoose;
 
 if (!cached) {
@@ -13,6 +9,10 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  if (!MONGODB_URI) {
+    console.warn("⚠️ MONGODB_URI is undefined. If this is a build step, it is safe to ignore.");
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
